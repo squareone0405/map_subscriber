1940b21a786def887ad69768a3d35471626d925e
squareone0405 <723172572@qq.com>
2018-09-08 00:05:12 +0800

    naive add

--- src/global_map.cpp
+++ src/global_map.cpp
@@ -2,13 +2,43 @@
 #include <iostream>
 
 using namespace std;
-/*
-void GlobalMap::tf_callback(const tf::tfMessage::ConstPtr &msg){
-    cout<<"get tffff"<<endl;
+
+void GlobalMap::tf_callback(const tf::tfMessage::ConstPtr &tf_msg){
+    auto transforms= tf_msg->transforms;
+    auto data_frame = string("undefined");
+    auto data = transforms[1].transform;
+    for(auto it = transforms.begin(); it!=transforms.end(); it++){
+        cout<<"frame id of tf_msg : "<<(*it).child_frame_id<<endl;
+        if((*it).child_frame_id == string("laser")){
+            data = (*it).transform;
+            data_frame = string("laser");
+        }
+    }
+    if(data_frame != string("laser")){
+        return;
+    }
+    x = data.translation.x;
+    y = data.translation.y;
+    z = data.translation.z;
+    cout<< x << '\t' << y << '\t' << z <<endl;
 }
-void GlobalMap::pointcloud_callback(const PointCloud2::scan_matched_points2 &msg){
-    cout<<"get pccccccc"<<endl;
+
+void GlobalMap::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg){
+    cout<<"height: "<<cloud_msg->height<<endl;
+    cout<<"width: "<<cloud_msg->width<<endl;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg (*cloud_msg, *temp_cloud);
+    Eigen::Matrix4f trans = Eigen::Matrix4f::Zero();
+    trans(0, 3) = x;
+    trans(1, 3) = y;
+    trans(2, 3) = z;
+    trans(3, 3) = 1;
+    pcl::transformPointCloud (*temp_cloud, *transformed_cloud, trans);
+    *cloud += *transformed_cloud;
+    counter++;
+    cout<<"counter: "<<counter<<endl;
+    if(counter == 20){
+        pcl::io::savePCDFileASCII ("/home/squareone/output.pcd", *cloud);
+    }
 }
-void GlobalMap::gridmap_callback(const OccupancyGrid::nav_msgs &msg){
-    cout<<"get gmmmmmmmmmmm"<<endl;
-}*/
--- src/global_map.h
+++ src/global_map.h
@@ -12,7 +12,9 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <Eigen/Dense>
 
+using namespace Eigen;
 using namespace std;
 
 class GlobalMap{
@@ -33,40 +35,11 @@ private:
     ros::Subscriber pointcloud_sub;
     ros::Subscriber gridmap_sub;
     double x, y, z;
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    void tf_callback(const tf::tfMessage::ConstPtr &tf_msg){
-        auto transforms= tf_msg->transforms;
-        auto data_frame = string("undefined");
-        auto data = transforms[1].transform;
-        for(auto it = transforms.begin(); it!=transforms.end(); it++){
-			cout<<"frame id of tf_msg : "<<(*it).child_frame_id<<endl;
-			if((*it).child_frame_id == string("laser")){
-				data = (*it).transform;
-				data_frame = string("laser");
-			}
-		}
-        if(data_frame != string("laser")){
-			return;
-		}
-        x = data.translation.x;
-        y = data.translation.y;
-        z = data.translation.z;
-        cout<< x << '\t' << y << '\t' << z <<endl;
-    }
-    void pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg){
-        cout<<"height: "<<cloud_msg->height<<endl;
-        cout<<"width: "<<cloud_msg->width<<endl;
-        pcl::PCLPointCloud2 pcl_pc2;
-        pcl_conversions::toPCL(*cloud_msg,pcl_pc2);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-        //pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
-        //pcl::PCLPointCloud2 pcl_pc2;
-        //pcl_conversions::toPCL(*cloud_msg,pcl_pc2);
-        //pcl::fromPCLPointCloud2(pcl_pc2, cloud);
-        //cout<<cloud.width<<endl;
-        //cout<<cloud.heitht<<endl;
-    }
+    int counter = 0;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+    void tf_callback(const tf::tfMessage::ConstPtr &tf_msg);
+    void pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg);
     void gridmap_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg){
-        cout<<"get gmmmmmmmmmmm"<<endl;
+        cout<<"get gridmap"<<endl;
     }
 };
