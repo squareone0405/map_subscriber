d3a2c6ae004be33b02739747daab0611e8fdf308
squareone0405 <723172572@qq.com>
2018-09-11 21:41:16 +0800

    add to_close_judge

--- src/global_map.cpp
+++ src/global_map.cpp
@@ -1,32 +1,54 @@
 #include "global_map.h"
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
-void GlobalMap::tf_callback(const tf::tfMessage::ConstPtr &tf_msg){
-    auto transforms = tf_msg->transforms;
-    auto transform = transforms[0].transform;
-    if(transforms[0].child_frame_id != string("curr_position"))
-        return;
-    x = transform.translation.x;
-    y = transform.translation.y;
-    z = transform.translation.z;
-    cout<< x << '\t' << y << '\t' << z <<endl;
+bool GlobalMap::isTooClose(geometry_msgs::Transform transform_new) {
+    float dis = pow(transform.translation.x - transform_new.translation.x, 2) 
+    + pow(transform.translation.y - transform_new.translation.y, 2) 
+    + pow(transform.translation.z - transform_new.translation.z, 2);
+    if(dis < 0.01)
+        return false;
+    float qx = transform_new.rotation.x;
+    float qy = transform_new.rotation.y;
+    float qz = transform_new.rotation.z;
+    float qw = transform_new.rotation.w;
+    float roll_new = std::atan2(2 * (qw * qx + qy * qz), (1 - 2 * (qx * qx + qy * qy)));
+    float pitch_new = std::asin(2 * (qw * qy - qx * qz));
+    float yaw_new = std::atan2(2 * (qw * qz + qx * qy), (1 - 2 * (qz * qz + qy * qy)));
+    float dis_angle = abs(roll_new - roll) + abs(pitch_new - pitch) + abs(yaw_new - yaw);
+    if(dis_angle < M_PI / 36)
+        return false;
+    transform = transform_new;
+    roll = roll_new;
+    pitch = pitch_new;
+    yaw = yaw_new;
+    return true;
 }
 
-void GlobalMap::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg){
+void GlobalMap::tf_callback(const tf::tfMessage::ConstPtr &tf_msg){
+    auto transforms = tf_msg->transforms;
+    auto transform_new = transforms[0].transform;
+    if(transforms[0].child_frame_id != string("curr_position"))
+        return;
+    if(isTooClose(transform_new))
+        return;    
+    cout<< transform.translation.x << '\t' << transform.translation.y << '\t' << transform.translation.z <<endl;
+}
+
+void GlobalMap::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg) {
+    if(!hasMoved)
+        return;
     pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    cout<<"init done"<<endl;
     pcl::fromROSMsg (*cloud_msg, *temp_cloud);
-    cout<<"parse msg"<<endl;
     Eigen::Matrix4f trans = Eigen::Matrix4f::Identity();
-    trans(0, 3) = x;
-    trans(1, 3) = y;
-    trans(2, 3) = z;
+    trans(0, 3) = transform.translation.x;
+    trans(1, 3) = transform.translation.y;
+    trans(2, 3) = transform.translation.z;
     pcl::transformPointCloud (*temp_cloud, *transformed_cloud, trans);
-    cout<<"transformed"<<endl;
     *cloud += *transformed_cloud;
     counter++;
     cout<<"num of clouds: "<<counter<<endl;
@@ -34,7 +56,7 @@ void GlobalMap::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cl
     cout<<"cloud height: "<<cloud->height<<endl;
     cout<<"cloud width: "<<cloud->width<<endl;
     if((counter%20) == 0){
-        pcl::io::savePCDFileASCII ("/home/odroid/Desktop/output" + std::to_string(counter) + ".pcd", *cloud);
+        pcl::io::savePCDFileASCII ("/home/odroid/Desktop/output/output" + std::to_string(counter) + ".pcd", *cloud);
         sensor_msgs::PointCloud2 msg;
         pcl::toROSMsg (*cloud, msg);
         pointcloud_pub.publish(msg);
--- src/global_map.h
+++ src/global_map.h
@@ -4,8 +4,8 @@
 #include <sensor_msgs/Imu.h>
 #include <tf/tfMessage.h>
 #include <geometry_msgs/Vector3.h>
-#include <geometry_msgs/Transform.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/Transform.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
@@ -19,10 +19,10 @@ using namespace std;
 class GlobalMap{
 public:
     GlobalMap(ros::NodeHandle &n){
+        roll = 0.0;
+        yaw = 0.0;
+        pitch = 0.0;
         cloud.reset((new pcl::PointCloud<pcl::PointXYZ>));
-        x = 0.0;
-        y = 0.0;
-        z = 0.0;
         tf_sub = n.subscribe("tf", 1000, &GlobalMap::tf_callback, this);
         pointcloud_sub = n.subscribe("scan_matched_points2", 1000, &GlobalMap::pointcloud_callback, this);
         pointcloud_pub = n.advertise<sensor_msgs::PointCloud2>("global_pointcloud", 1000);
@@ -36,9 +36,12 @@ private:
     ros::Subscriber tf_sub;
     ros::Subscriber pointcloud_sub;
     ros::Publisher pointcloud_pub;
-    float x, y, z;
+    geometry_msgs::Transform transform;
+    bool hasMoved;
+    float roll, yaw, pitch;
     int counter = 0;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
     void tf_callback(const tf::tfMessage::ConstPtr &tf_msg);
     void pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg);
+    bool isTooClose(geometry_msgs::Transform transform_new);
 };
