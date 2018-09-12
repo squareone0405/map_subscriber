a047406ecba35ee91e5c393c914d308e46eee20e
squareone0405 <723172572@qq.com>
2018-09-12 10:36:38 +0800

    debugging

--- src/global_map.cpp
+++ src/global_map.cpp
@@ -9,7 +9,8 @@ bool GlobalMap::isTooClose() {
     float dis = pow(transform_prev.translation.x - transform_curr.translation.x, 2) 
     + pow(transform_prev.translation.y - transform_curr.translation.y, 2) 
     + pow(transform_prev.translation.z - transform_curr.translation.z, 2);
-    if(dis < 0.01)
+    cout<<"dis: "<<dis<<endl;
+    if(dis < 0.0025)
         return true;
     float qx = transform_curr.rotation.x;
     float qy = transform_curr.rotation.y;
@@ -19,6 +20,7 @@ bool GlobalMap::isTooClose() {
     float pitch_new = std::asin(2 * (qw * qy - qx * qz));
     float yaw_new = std::atan2(2 * (qw * qz + qx * qy), (1 - 2 * (qz * qz + qy * qy)));
     float dis_angle = abs(roll_new - roll) + abs(pitch_new - pitch) + abs(yaw_new - yaw);
+    cout<<"dis_angle: "<<dis_angle<<endl;
     if(dis_angle < M_PI / 36)
         return true;
     transform_prev = transform_curr;
@@ -29,12 +31,13 @@ bool GlobalMap::isTooClose() {
 }
 
 void GlobalMap::tf_callback(const tf::tfMessage::ConstPtr &tf_msg){
+    cout<<"tf_begin"<<endl;
     auto transforms = tf_msg->transforms;
     auto transform_new = transforms[0].transform;
     if(transforms[0].child_frame_id != string("curr_position"))
         return;
     transform_curr = transform_new;
-    //cout<< transform.translation.x << '\t' << transform.translation.y << '\t' << transform.translation.z <<endl;
+    cout<<"tf_done"<<endl;
 }
 
 void GlobalMap::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg) {
@@ -46,17 +49,18 @@ void GlobalMap::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr &cl
     pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg (*cloud_msg, *temp_cloud);
     Eigen::Matrix4f trans = Eigen::Matrix4f::Identity();
-    trans(0, 3) = transform_curr.translation.x;
-    trans(1, 3) = transform_curr.translation.y;
-    trans(2, 3) = transform_curr.translation.z;
+    trans(0, 3) = transform_prev.translation.x;
+    trans(1, 3) = transform_prev.translation.y;
+    trans(2, 3) = transform_prev.translation.z;
+    cout<< transform_prev.translation.x << '\t' << transform_prev.translation.y << '\t' << transform_prev.translation.z <<endl;
     pcl::transformPointCloud (*temp_cloud, *transformed_cloud, trans);
     *cloud += *transformed_cloud;
     counter++;
-    cout<<"num of clouds: "<<counter<<endl;
+    /*cout<<"num of clouds: "<<counter<<endl;
     cout<<"num of points: "<<cloud->points.size()<<endl;
     cout<<"cloud height: "<<cloud->height<<endl;
-    cout<<"cloud width: "<<cloud->width<<endl;
-    if((counter%2) == 0){
+    cout<<"cloud width: "<<cloud->width<<endl;*/
+    if((counter%10) == 0){
         pcl::io::savePCDFileASCII ("/home/odroid/Desktop/output/output" + std::to_string(counter) + ".pcd", *cloud);
         sensor_msgs::PointCloud2 msg;
         pcl::toROSMsg (*cloud, msg);
